523011da3a4df0a7e9009eea8fe061d2de2c8e72
italolourenco <italolt10@gmail.com>
2020-06-14 00:13:06 -0300

    fix: remove logs

--- src/routes/ScraperRouter.ts
+++ src/routes/ScraperRouter.ts
@@ -4,7 +4,6 @@ const scraperRouter = Router()
 
 scraperRouter.get('/:username/:repositoryname', async (request, response) => {
     const { username, repositoryname } = request.params
-    console.log(username, repositoryname)
     return response.status(200).json({message : 'OK'})
 
 })
