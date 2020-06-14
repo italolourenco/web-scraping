183f49d099d727db8eeddcc2eabc6a7d6acf0a4a
italolourenco <italolt10@gmail.com>
2020-06-14 09:40:20 -0300

    refactor: changing scraper router to use scraper service

--- src/routes/ScraperRouter.ts
+++ src/routes/ScraperRouter.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express'
+import ScraperService from '../services/ScraperService'
 
 const scraperRouter = Router()
 
+const GIT_HUB_PATH = 'https://github.com'
+
 scraperRouter.get('/:username/:repositoryname', async (request, response) => {
     const { username, repositoryname } = request.params
+
+    await ScraperService.execute(username, repositoryname)
+
     return response.status(200).json({message : 'OK'})
 
 })
